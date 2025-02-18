fa7349795dfa41b8d2a9865382da4566cb7ebd9b
zionmezba <mezbazion@gmail.com>
2025-02-18 09:32:24 +0600

    intit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LabClass05());
+}
+
+class LabClass05 extends StatelessWidget {
+  const LabClass05({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Basic Flutter UI - 02",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Basic Flutter UI - 02"),
+        ),
+        body: Text(
+          "Basic Flutter UI - Part 02",
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
