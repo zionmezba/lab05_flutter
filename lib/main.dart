fd7cd5bb37daf85b2d36a9d69379f373e76b7080
zionmezba <mezbazion@gmail.com>
2025-02-19 11:59:00 +0600

    as

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,18 @@ void main() {
   runApp(Lab05());
 }
 
+/*
+
+ Handling Overflow
+ 1. Single Child Scroll View
+  - Scrollable
+ 2. Expanded
+ - Overlaps if needed
+ 3. Flexible
+ - Overlaps if needed but with more customization
+
+*/
+
 class Lab05 extends StatelessWidget {
   const Lab05({super.key});
 
@@ -14,35 +26,26 @@ class Lab05 extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text("Flutter UI 2"),
+          backgroundColor: Colors.blue,
         ),
-        body: Row(
-          children: [
-            Icon(
-              Icons.star,
-              color: Colors.yellow,
-              size: 60,
+        body: Container(
+          // height: double.infinity,
+          width: double.infinity,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: List.generate(
+                100,
+                (index) => Icon(
+                  Icons.radar,
+                  color: Colors.blue,
+                  size: index.toDouble(),
+                ),
+              ),
             ),
-            Icon(
-              Icons.star,
-              color: Colors.yellow,
-              size: 60,
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.grey,
-              size: 60,
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.grey,
-              size: 60,
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.grey,
-              size: 60,
-            ),
-          ],
+          ),
         ),
       ),
     );
