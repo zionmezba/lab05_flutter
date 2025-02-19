0055be36b1ad927cbf1c2878af50fad34c320b53
zionmezba <mezbazion@gmail.com>
2025-02-19 09:53:20 +0600

    task 01

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const LabClass05());
+  runApp(Lab05());
 }
 
-class LabClass05 extends StatelessWidget {
-  const LabClass05({super.key});
+class Lab05 extends StatelessWidget {
+  const Lab05({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic Flutter UI - 02",
+      title: "Flutter UI 2",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Basic Flutter UI - 02"),
-          backgroundColor: Colors.blue[400],
-          centerTitle: true,
+          title: Text("Flutter UI 2"),
         ),
-        body: Text("Updated something"),
+        body: Row(
+          children: [
+            Icon(
+              Icons.star,
+              color: Colors.yellow,
+              size: 60,
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.yellow,
+              size: 60,
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.grey,
+              size: 60,
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.grey,
+              size: 60,
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.grey,
+              size: 60,
+            ),
+          ],
+        ),
       ),
     );
   }
