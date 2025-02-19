8726bfd65abf316fef5fe54d09f1c43316684128
zionmezba <mezbazion@gmail.com>
2025-02-19 09:05:24 +0600

    fresh

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,6 @@ void main() {
   runApp(const LabClass05());
 }
 
-/* 
-Overflow Handling
-1. Single Child Scroll View
-2. Expanded
-3. Flexible
-
-*/
-
 class LabClass05 extends StatelessWidget {
   const LabClass05({super.key});
 
@@ -26,54 +18,7 @@ class LabClass05 extends StatelessWidget {
           backgroundColor: Colors.blue[400],
           centerTitle: true,
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Icon(
-              Icons.abc_sharp,
-              size: 50,
-              color: const Color.fromARGB(255, 255, 0, 0),
-            ),
-            Icon(
-              Iconsax.activity,
-              size: 50,
-              color: const Color.fromARGB(255, 255, 0, 0),
-            ),
-            Row(
-              children: [
-                Icon(
-                  Iconsax.activity,
-                  size: 50,
-                  color: const Color.fromARGB(255, 255, 238, 0),
-                ),
-                Icon(
-                  Iconsax.activity,
-                  size: 50,
-                  color: const Color.fromARGB(255, 255, 238, 0),
-                ),
-                Icon(
-                  Iconsax.activity,
-                  size: 50,
-                  color: const Color.fromARGB(255, 255, 238, 0),
-                ),
-                Column(
-                  children: [
-                    Icon(
-                      Iconsax.activity,
-                      size: 50,
-                      color: const Color.fromARGB(255, 4, 0, 255),
-                    ),
-                    Icon(
-                      Iconsax.activity,
-                      size: 50,
-                      color: const Color.fromARGB(255, 0, 110, 255),
-                    ),
-                  ],
-                )
-              ],
-            )
-          ],
-        ),
+        body: Text("Flutter Basics 02"),
       ),
     );
   }
